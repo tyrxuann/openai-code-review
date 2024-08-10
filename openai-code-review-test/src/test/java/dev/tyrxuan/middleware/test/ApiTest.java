dd6351833ebbc7d88d579ea09986a177ca667dd2
Mercy Wang <wanghongxuan36@gmail.com>
2024-08-11 01:09:51 +0800

    feat: test code review

--- openai-code-review-test/src/test/java/dev/tyrxuan/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/dev/tyrxuan/middleware/test/ApiTest.java
@@ -13,8 +13,29 @@ public class ApiTest {
         log.info("context load...");
     }
 
+    /**
+     * 测试冒泡排序算法的正确性。
+     */
     @Test
-    public void test() {
-        System.out.println(Integer.parseInt("12345678"));
+    public void testBubbleSort() {
+        // 待排序的数组
+        int[] array = {1, 3, 2, 5, 4};
+
+        // 冒泡排序算法
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                // 如果当前元素大于下一个元素，则交换它们的位置
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        // 打印排序后的数组
+        for (int value : array) {
+            System.out.println(value);
+        }
     }
 }
